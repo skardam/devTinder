9ab1261ec115256112860b0390f4abe519bd477c
skardam <sushikardam@gmail.com>
2025-01-27 17:52:20 +0530

    exploring get put post etc

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(function (req, res) {
-    res.send("Response from server");
+app.get('/users', (req, res)=>{
+    res.send({ fullName: 'sushmita', address: 'bangalore' });
+})
+app.post('/users', (req, res)=>{
+    res.send({ fullName: 'Abhishek', address: 'bangalore' });
+})
+app.put('/users', (req, res)=>{
+    res.send({ fullName: 'sachin', address: 'bangalore' });
+})
+app.patch('/users', (req, res)=>{
+    res.send({address: 'bangalore' });
 })
 app.listen(3000, () => {
     console.log('listening on port 3000');
