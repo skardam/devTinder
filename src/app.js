28c9306c6973caaf7383cab4ddd515c68e6d45f8
skardam <sushikardam@gmail.com>
2025-01-30 14:45:29 +0530

    added get and delete APIs

--- src/app.js
+++ src/app.js
@@ -3,15 +3,51 @@ const app = express();
 const connectDB = require('./config/database');
 const User = require('./models/user');
 
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailID;
+    try {
+        const user = await User.find({ emailID: userEmail });
+        if(!user){
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }      
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if(!users){
+            res.status(404).send("No User found");
+        } else {
+            res.send(users);
+        }      
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        await User.findOneAndDelete(userId);
+        res.send("User deleted successfully");
+    }
+     catch(err){
+        res.status(500).send(err);
+    }
+   
+})
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Bholu",
-        lastName: "baghel",
-        emailID: "bholu.baghel@gmail.com",
-        password: "password",
-        age: 27,
-        gender: "male"
-    });
+    const user = new User(req.body);
     await user.save();
     res.send("User created successfully");
 })
